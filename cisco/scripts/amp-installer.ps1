c745046707f52650b5f113e5de1ead60cafc92ab
yalmodovar <102333119+yalmodovar@users.noreply.github.com>
2022-06-02 10:45:49 -0700

    Updated NAS info

--- cisco/scripts/amp-installer.ps1
+++ cisco/scripts/amp-installer.ps1
@@ -1,5 +1,5 @@
-net use y: $env:nas_path\iaas /user:$env:mount_username $env:mount_password /persistent:no
-xcopy y:\selfserviceimages\amp c:\temp\amp\ /s /e 
+net use y: $env:nas_path\CGOAnsible$ /user:$env:mount_username $env:mount_password /persistent:no
+xcopy y:\Cisco_AMP\amp_Windows_Servers-PROTECT.exe c:\temp\amp\ /s /e 
 net use y: /d 
 c:\\temp\\amp\\amp_Windows_Servers-PROTECT.exe /R /S
 Start-Sleep 300
